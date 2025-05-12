65301c20d5008990fa0f5f1e0794d361ea6f9e3a
Arisha002 <bscs2312358@szabist.pk>
2025-05-12 08:42:59 +0000

    updated me

--- Studentweb/js/count.js
+++ Studentweb/js/count.js
@@ -1,18 +1,70 @@
 const employee_API_LINK = "https://congenial-memory-q765wgppx7x9f6wwj-5001.app.github.dev/employees";
+const regions_API_LINK = "https://congenial-memory-q765wgppx7x9f6wwj-5001.app.github.dev/regions";
+const jobs_API_LINK = "https://congenial-memory-q765wgppx7x9f6wwj-5001.app.github.dev/jobs1";
+
 
 fetch(employee_API_LINK).then(response=>{
     if(!response.ok)
         throw new Error("Failed to fetch data");
     return response.json();
-}).then(data=>{
+}).then(data => {
   
     const tbody = document.querySelector("#Cemployees");
     
-    data.forEach(job=>{
-        const row = document.createElement("employee-count");
+    data.forEach(employee1=>{
+        const row = document.createElement("employee-container");
 
         row.innerHTML=`
-        <p>Count : ${job.count}</p>
+        <p>Count : ${employee1.count}</p>
+        `;
+      
+        tbody.appendChild(row);
+    });
+
+}).catch(err=>{
+    console.log(err.message);
+});
+
+
+
+
+fetch(regions_API_LINK).then(response=>{
+    if(!response.ok)
+        throw new Error("Failed to fetch data");
+    return response.json();
+}).then(data => {
+  
+    const tbody = document.querySelector("#Cregions");
+    
+    data.forEach(regions=>{
+        const row = document.createElement("regions-container");
+
+        row.innerHTML=`
+        <p>Count : ${regions.count}</p>
+        `;
+      
+        tbody.appendChild(row);
+    });
+
+}).catch(err=>{
+    console.log(err.message);
+});
+
+
+
+fetch(jobs_API_LINK).then(response=>{
+    if(!response.ok)
+        throw new Error("Failed to fetch data");
+    return response.json();
+}).then(data => {
+  
+    const tbody = document.querySelector("#Cjobs");
+    
+    data.forEach(jobs=>{
+        const row = document.createElement("jobs-container");
+
+        row.innerHTML=`
+        <p>Count : ${jobs.count}</p>
         `;
       
         tbody.appendChild(row);
